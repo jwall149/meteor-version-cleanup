abd13f40aba8b45da3598f9b505ee89f989d7e6d
Tonny Pham <jwall149@gmail.com>
2016-03-15 17:50:11 +0900

    Clean up meteor/version when new client install/update

--- src/android/MeteorCleanupPlugin.java
+++ src/android/MeteorCleanupPlugin.java
@@ -1,21 +1,55 @@
 package me.tonny.cordova.meteorcleanup;
 
-import org.apache.cordova.CallbackContext;
+import android.content.Context;
+import android.util.Log;
+
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
 
+import java.io.File;
 
 public class MeteorCleanupPlugin extends CordovaPlugin {
 
-  @Override
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 
-    super.initialize(cordova, webView);
-  }
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        String version;
+        try {
+            version = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName;
+        } catch (Exception e) {
+            return;
+        }
+        String key = "esportsplus_mcp_version_mark_" + version;
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
+        if (!sharedPrefs.contains(key)) {
+            Context context = cordova.getActivity();
+            String dataDirectory = context.getFilesDir().toString();
+            String meteorVersion = dataDirectory + "/meteor/version";
+
+            try {
+                File mvf = new File(meteorVersion);
+                boolean noError = true;
+                if (mvf.exists()) {
+                    noError = mvf.delete();
+                }
+                if (noError) {
+                    Editor editor = sharedPrefs.edit();
+                    editor.putString(key, "marked");
+                    editor.commit();
+                }
+            } catch (Exception e) {
+                Log.d("MeteorCleanupPlugin", "Error while check meteor version files");
+            }
+        }
+        super.initialize(cordova, webView);
+    }
 
 }
--- src/ios/MeteorCleanupPlugin.m
+++ src/ios/MeteorCleanupPlugin.m
@@ -5,6 +5,29 @@
 - (CDVPlugin *)initWithWebView:(UIWebView *)theWebView {
     NSLog(@"Checking cordova version");
     NSString* version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString* key = [NSString stringWithFormat:@"esportsplus_mcp_version_mark_%@", version];
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+
+    id settingsValue = [defaults objectForKey:key];
+
+    if (settingsValue == nil) {
+        NSLog(@"This version %@ is first time install/downloaded, remove meteor current version...", version);
+
+        NSString* libPath = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES)[0];
+        NSString* meteorVersionPath = [libPath stringByAppendingPathComponent:@"NoCloud/meteor/version"];
+
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        BOOL hasMeteorVersion = [fileManager fileExistsAtPath:meteorVersionPath];
+        NSError* error;
+        if (hasMeteorVersion) {
+            [fileManager removeItemAtPath:meteorVersionPath error:&error];
+        }
+        if (!error) {
+            [defaults setValue:@"marked" forKey:key];
+            [defaults synchronize];
+        }
+    }
 
     return self;
 }
